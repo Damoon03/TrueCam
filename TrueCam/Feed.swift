8965f54310f85e9bbcc3bc4051a4af28cc3b6162
Damoon Saber <damoonsaber@gmail.com>
2026-05-13 20:40:16 +0330

    finishing Feed and inserting FeedCell

--- TrueCam/Feed.swift
+++ TrueCam/Feed.swift
@@ -13,12 +13,68 @@ struct Feed: View {
             Color.black
                 .ignoresSafeArea()
             ZStack {
+                ScrollView {
+                    VStack {
+                        VStack {
+                            ZStack {
+                                VStack(alignment: .leading) {
+                                    Image("back")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .cornerRadius(5)
+                                }
+                                
+                                VStack {
+                                    HStack {
+                                        Image("front")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .border(.black)
+                                            .cornerRadius(2)
+                                            .frame(width: 20, height: 40)
+                                            .padding(.leading)
+                                        Spacer()
+                                        
+                                        
+                                    }
+                                    .padding(.top, 18)
+                                    Spacer()
+                                }
+                            }
+                            .frame(width: 100)
+                        }
+                        
+                        VStack { Text("Add a Caption...")
+                                .foregroundStyle(Color.white)
+                                .fontWeight(.semibold)
+                            
+                            Text("View Comment")
+                                .foregroundStyle(Color.gray)
+                            
+                            HStack {
+                                
+                                Text("Iran, Rasht • 2h ago")
+                                    .foregroundStyle(Color.gray)
+                                    .font(.system(size: 12))
+                                
+                                Image(systemName: "ellipsis")
+                                    .foregroundStyle(Color.gray)
+                            }
+                        }
+                        
+                        ForEach(1 ..< 8) { _ in
+                            FeedCell()
+                        }
+                    }
+                    .padding(.top, 80)
+                }
+                
                 VStack {
                     VStack {
                         HStack {
-                            Image(systemName: "camera.aperture")
+                            Image(systemName: "person.2.fill")
                                 .foregroundColor(.white)
-                           
+                            
                             Spacer()
                             
                             Text("TrueCam")
@@ -40,7 +96,7 @@ struct Feed: View {
                         HStack {
                             Text("My Friends")
                                 .foregroundStyle(Color(.white))
-
+                            
                             Text("Discovery")
                                 .foregroundStyle(Color(.gray))
                         }
